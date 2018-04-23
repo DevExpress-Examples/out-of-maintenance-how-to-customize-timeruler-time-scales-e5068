024b76b4fd8b9181da7020d2da4542ef698e8418
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/CustomTimeRulerSample/MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using DevExpress.Xpf.Scheduler;
+using DevExpress.XtraScheduler.Services;
+using System;
+using System.Windows;
+
+namespace DXSample {
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window {
+        public MainWindow() {
+            InitializeComponent();
+
+            // Substitute the service to change DateTime format strings for the TimeRuler.
+            #region #replaceservice
+            ITimeRulerFormatStringService svc = scheduler.GetService<ITimeRulerFormatStringService>();
+            scheduler.ReplaceService<ITimeRulerFormatStringService>(new MyTimeRulerFormatStringService(svc));
+            #endregion #replaceservice
+
+            // Add a new TimeRuler with the specified TimeZone.
+            #region #newtimeruler
+            SchedulerTimeRuler rulerUTC = new SchedulerTimeRuler();
+            rulerUTC.TimeZoneId = TimeZoneInfo.Utc.Id;
+            rulerUTC.Caption = "UTC";
+            rulerUTC.TimeMarkerVisibility = DevExpress.XtraScheduler.TimeMarkerVisibility.Never;
+            scheduler.DayView.TimeRulers.Add(rulerUTC);
+            #endregion #newtimeruler
+
+            SchedulerTimeRuler rulerLocal = new SchedulerTimeRuler();
+            rulerLocal.UseClientTimeZone = true;
+            rulerLocal.Caption = "Local";
+            scheduler.DayView.TimeRulers.Add(rulerLocal);
+
+            #region #timeslots
+            // Add 20-minute time slots to the view.
+            scheduler.DayView.TimeSlots.AddRange(scheduler.DayView.DefaultTimeSlots);
+            scheduler.DayView.TimeSlots.Add(new DevExpress.XtraScheduler.TimeSlot(new TimeSpan(0, 20, 0), "20 minutes"));
+            #endregion #timeslots
+
+        }
+    }
+}
---
+++ VB/CustomTimeRulerSample/MainWindow.xaml.vb
@@ -0,0 +1,44 @@
+﻿Imports DevExpress.Xpf.Scheduler
+Imports DevExpress.XtraScheduler.Services
+Imports System
+Imports System.Windows
+
+Namespace DXSample
+    ''' <summary>
+    ''' Interaction logic for MainWindow.xaml
+    ''' </summary>
+    Partial Public Class MainWindow
+        Inherits Window
+
+        Public Sub New()
+            InitializeComponent()
+
+            ' Substitute the service to change DateTime format strings for the TimeRuler.
+'            #Region "#replaceservice"
+            Dim svc As ITimeRulerFormatStringService = scheduler.GetService(Of ITimeRulerFormatStringService)()
+            scheduler.ReplaceService(Of ITimeRulerFormatStringService)(New MyTimeRulerFormatStringService(svc))
+'            #End Region ' #replaceservice
+
+            ' Add a new TimeRuler with the specified TimeZone.
+'            #Region "#newtimeruler"
+            Dim rulerUTC As New SchedulerTimeRuler()
+            rulerUTC.TimeZoneId = TimeZoneInfo.Utc.Id
+            rulerUTC.Caption = "UTC"
+            rulerUTC.TimeMarkerVisibility = DevExpress.XtraScheduler.TimeMarkerVisibility.Never
+            scheduler.DayView.TimeRulers.Add(rulerUTC)
+'            #End Region ' #newtimeruler
+
+            Dim rulerLocal As New SchedulerTimeRuler()
+            rulerLocal.UseClientTimeZone = True
+            rulerLocal.Caption = "Local"
+            scheduler.DayView.TimeRulers.Add(rulerLocal)
+
+'            #Region "#timeslots"
+            ' Add 20-minute time slots to the view.
+            scheduler.DayView.TimeSlots.AddRange(scheduler.DayView.DefaultTimeSlots)
+            scheduler.DayView.TimeSlots.Add(New DevExpress.XtraScheduler.TimeSlot(New TimeSpan(0, 20, 0), "20 minutes"))
+'            #End Region ' #timeslots
+
+        End Sub
+    End Class
+End Namespace
