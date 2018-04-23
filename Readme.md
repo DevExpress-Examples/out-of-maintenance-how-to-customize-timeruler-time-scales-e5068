3e90da94932ebd9735c8936cab4250ce1a9f8c9b
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ Readme.md
@@ -0,0 +1,8 @@
+# How to customize TimeRuler time scales
+
+
+<p>This example demonstrates how you can specify format string for displaying time in the TimeRuler's time scale and add a custom (20 min) time slot.</p>
+
+<br/>
+
+
---
+++ VB/CustomTimeRulerSample/MyTimeRulerFormatStringService.vb
@@ -0,0 +1,34 @@
+﻿Imports Microsoft.VisualBasic
+Imports DevExpress.XtraScheduler
+Imports DevExpress.XtraScheduler.Services
+Imports System
+Imports System.Collections.Generic
+Imports System.Linq
+Imports System.Text
+Imports System.Threading.Tasks
+
+Namespace DXSample
+#Region "#myservice    "
+Public Class MyTimeRulerFormatStringService
+	Inherits TimeRulerFormatStringServiceWrapper
+		Public Sub New(ByVal service As ITimeRulerFormatStringService)
+			MyBase.New(service)
+		End Sub
+		Public Overrides Function GetHalfDayHourFormat(ByVal ruler As TimeRuler) As String
+			Return If(ruler.UseClientTimeZone, "htt", "HH:mm")
+		End Function
+		Public Overrides Function GetHourFormat(ByVal ruler As TimeRuler) As String
+			Return If(ruler.UseClientTimeZone, "htt", "HH:mm")
+		End Function
+		Public Overrides Function GetHourOnlyFormat(ByVal ruler As TimeRuler) As String
+			Return If(ruler.UseClientTimeZone, "htt", "HH")
+		End Function
+		Public Overrides Function GetMinutesOnlyFormat(ByVal ruler As TimeRuler) As String
+			Return If(ruler.UseClientTimeZone, "''", "mm")
+		End Function
+		Public Overrides Function GetTimeDesignatorOnlyFormat(ByVal ruler As TimeRuler) As String
+			Return If(ruler.UseClientTimeZone, "''", "mm")
+		End Function
+End Class
+#End Region ' #myservice
+End Namespace
